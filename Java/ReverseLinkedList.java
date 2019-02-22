88b21802a3e7507013d750faf06be441a91aa737
rohan7t <rohan5393@gmail.com>
2019-02-21 22:52:47 -0500

    Day progress

---
+++ Java/ReverseLinkedList.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class ReverseLinkedList {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public ListNode reverseList(ListNode head) {
+		ListNode prev = null;
+		ListNode next = null;
+		while (head != null) {
+			next = head.next;
+			head.next = prev;
+			prev = head;
+			head = next;
+		}
+		return prev;
+	}
+
+}
