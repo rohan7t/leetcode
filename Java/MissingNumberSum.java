44a5a2e0ec7e4bc3a8e84e2daebb9cb31c536365
Rohan Tigadi <rohan5393@gmail.com>
2019-03-16 03:32:14 -0400

    missing number sum

---
+++ Java/MissingNumberSum.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class MissingNumberSum {
+    public int missingNumber(int[] nums) {
+	int length = nums.length;
+	int expectedSum = length * (length + 1) / 2;
+	int actualSum = 0;
+	for (int num : nums) {
+	    actualSum += num;
+	}
+	return expectedSum - actualSum;
+    }
+}
