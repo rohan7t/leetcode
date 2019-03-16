2485f8753e6a919ccd0d679061e67709fd203304
Rohan Tigadi <rohan5393@gmail.com>
2019-03-16 15:43:57 -0400

    single number

---
+++ Java/SingleNumber.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+	if (nums == null || nums.length == 0) {
+	    return 0;
+	}
+	int result = nums[0];
+	for (int i = 1; i < nums.length; i++) {
+	    result ^= nums[i];
+	}
+	return result;
+    }
+}
