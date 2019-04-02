776219f18a8224b7d3a578d1fd9def5fd3484d8b
Rohan Tigadi <rohan5393@gmail.com>
2019-04-01 21:01:30 -0400

    Find disappeared numbers

---
+++ Java/FindDisappearedNumbers.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindDisappearedNumbers {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+	List<Integer> result = new ArrayList<Integer>();
+
+	for (int i = 0; i < nums.length; i++) {
+	    int val = Math.abs(nums[i]) - 1;
+	    if (nums[val] > 0) {
+		nums[val] = -nums[val];
+	    }
+	}
+	for (int i = 0; i < nums.length; i++) {
+	    if (nums[i] > 0) {
+		result.add(i + 1);
+	    }
+	}
+	return result;
+    }
+}
